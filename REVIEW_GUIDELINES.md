7ee0c6e65536457b32853b9b8cc084ea022a4a70
ramnath <raman9999@gmail.com>
2025-09-05 07:57:10 +0530

    Update assembly informational version and add review guidelines document

---
+++ REVIEW_GUIDELINES.md
@@ -0,0 +1,4 @@
+# Review Guidelines for Loan Origination System
+
+1. Entity naming convention: ends with "Entity".
+2. Employee null value validation must be done before using it.
--- obj/Debug/net8.0/EmployeeServiceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeServiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7291d7eebe571c9fa9395bd24ccd8a7278c2bfa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bcaefde05d58769dae34b47b5a4f1ce6d348e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
